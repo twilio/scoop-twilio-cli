9645d1cd8266c5ab97fbf9ae50d55b0bc3cae81b
Anuj <abadhwar@twilio.com>
2021-11-18 23:38:05 +0530

    update version fixes

--- bin/updatever.ps1
+++ bin/updatever.ps1
@@ -1,4 +1,4 @@
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 $dir = "$psscriptroot/../" # checks the parent dir
-Invoke-Expression -command "& '$checkver' -dir '$dir' $($args | ForEach-Object { "$_ " })"
+Invoke-Expression -command "& '$checkver' -dir '$dir' $($args | ForEach-Object { "$_ " }) -Update"
