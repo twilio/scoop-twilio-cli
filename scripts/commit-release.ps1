5227b90d70f02f3a127a0d3512cb2f8ce4725166
abadhwar <abadhwar@twilio.com>
2021-11-19 01:10:26 +0530

    Refactor bin to scripts

--- scripts/commit-release.ps1
+++ scripts/commit-release.ps1
@@ -5,10 +5,10 @@ param(
 echo "Git configurations"
 git config --global user.email "team_interfaces+github@twilio.com"
 git config --global user.name "twilio-dx"
-$branch=git branch --show-current
-
 git add .\twilio.json
 $git_status=git status --porcelain
+$branch=git branch --show-current
+
 if ($git_status -ne $null) {
     git commit -m "Update manifest to version $version"
     git push origin "$branch"
