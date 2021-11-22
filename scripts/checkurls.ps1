ebc39e2ddad0ae58f34c63d460f9cdecc210a08a
Anuj Badhwar <anuj.badhwar@outlook.com>
2021-11-22 16:31:12 +0530

    Revert "chore: Add scoop github actions workflow"

--- scripts/checkurls.ps1
+++
@@ -1,4 +0,0 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$checkurls = "$env:SCOOP_HOME/bin/checkurls.ps1"
-$dir = "$psscriptroot/../" # checks the parent dir
-Invoke-Expression -command "& '$checkurls' -dir '$dir' $($args | ForEach-Object { "$_ " })"
--- scripts/updatever.ps1
+++
@@ -1,4 +0,0 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
-$checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
-$dir = "$psscriptroot/../" # checks the parent dir
-Invoke-Expression -command "& '$checkver' -dir '$dir' $($args | ForEach-Object { "$_ " }) -Update"
