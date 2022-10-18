bd45bfd4911d6c1668eec9982679b168baa87c70
Isha Bansal <ishabansal2467@gmail.com>
2022-10-18 20:19:04 +0530

    fix: update urls

--- scripts/updatever.ps1
+++ scripts/updatever.ps1
@@ -1,4 +1,11 @@
+param(
+    [String] $version = ''
+)
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
 $dir = "$psscriptroot/../" # checks the parent dir
-Invoke-Expression -command "& '$checkver' -dir '$dir' $($args | ForEach-Object { "$_ " }) -Update"
+if($version -like "*draft*"){
+    Invoke-Expression -command "& '$checkver' -a '$psscriptroot/../twiliodraft.json' -Update -v '$version'"
+}else{
+    Invoke-Expression -command "& '$checkver' -dir '$dir' $($args | ForEach-Object { "$_ " }) -Update"
+}
